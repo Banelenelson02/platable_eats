cb92f90394bdcf854c840f19445a4bd845f2a517
Banele Nelson Ntuli <banelenelson02@gmail.com>
2026-08-06 10:36:19 +0200

    just a message nothing much

--- src/main/java/com/plateable/dto/request/CreateOrderRequest.java
+++ src/main/java/com/plateable/dto/request/CreateOrderRequest.java
@@ -1,7 +1,7 @@
 package com.plateable.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-
+//this is just a comment mize it
 public record CreateOrderRequest(
         @NotBlank(message = "tableId is required") String tableId,
         @NotBlank(message = "waiterId is required") String waiterId
